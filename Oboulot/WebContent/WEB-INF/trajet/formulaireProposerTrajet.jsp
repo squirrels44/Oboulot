82d7686cd9fbdf0cd5e0ac56dc6e1dfe4cf57596
active-admin <active-admin@LAPTOP-6HG7OFNT>
2016-08-24 15:29:41 +0200

    ajout des option fumeur et musique dans le formulaire + servlet pas totalement terminée

--- Oboulot/WebContent/WEB-INF/trajet/formulaireProposerTrajet.jsp
+++ Oboulot/WebContent/WEB-INF/trajet/formulaireProposerTrajet.jsp
@@ -1,19 +1,18 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Proposer un trajet</title>
 </head>
 <body>
 	<form method="POST" action="formPropositionTrajet">
 			<p>Vous pouvez proposer votre trajet via ce formulaire.</p>
 			<div>
-				<label for="ptdepart">Adresse de départ<span class="requis">*</span></label> 
+				<label for="ptdepart">Adresse de dÃ©part<span class="requis">*</span></label> 
 				<input type="text" name="ptdepart" value="${form['ptdepart']}"/><span class="error">${erreurs['ptdepart']}</span><br />
 				
-				<label for="ptarrivee">Adresse d'arrivée<span class="requis">*</span></label>
+				<label for="ptarrivee">Adresse d'arrivÃ©e<span class="requis">*</span></label>
 				<input type="text" name="ptarrivee" value="${form['ptarrivee']}" /> <span class="error">${erreurs['ptarrivee']}</span> <br />
 				
 				<label for="ptpassage">Adresse d'un point de passage</label>
@@ -22,11 +21,23 @@
 				<label for="date">Date du trajet<span class="requis">*</span></label>
 				<input type="text" name="date" value="${form['date']}" /> <span class="error">${erreurs['date']}</span> <br />
 				
-				<label for="heure">Heure de départ<span class="requis">*</span></label>
+				<label for="heure">Heure de dÃ©part<span class="requis">*</span></label>
 				<input type="text" name="heure" value="${form['heure']}" /> <span class="error">${erreurs['heure']}</span> <br />
 		
 				<label for="nbplace">Nombre de places disponibles<span class="requis">*</span></label> 
 				<input type="text" name="nbplace" value="${form['nbplace']}" /><br />
+				
+				<label for="fumeur">Fumeur :</label>
+				<input type="radio" name="fumeur" value="NonFumeur" checked>
+				<label for="NonFumeur"><img src="/WEB-INF/images/logo_non_fumeur.jpg" alt="Non fumeur" /></label>
+				<input type="radio" name="fumeur" value="Fumeur">
+				<label for="Fumeur"><img src="/WEB-INF/images/logo_fumeur.jpg" alt="Fumeur" /></label><br />
+				
+				<label for="musique">Musique :</label>
+				<input type="radio" name="musique" value="NonMusique" checked>
+				<label for="NonMusique">Sans musique</label>
+				<input type="radio" name="musique" value="AvecMusique">
+				<label for="AvecMusique">Avec musique</label><br />
 				
 				<label for="submit"></label> 
 				<input type="submit" name="submit" value="Valider mon trajet" />
---
+++ Oboulot/src/com/squirrels/oboulot/ctrl/ProposerTrajet.java
@@ -0,0 +1,246 @@
+package com.squirrels.oboulot.ctrl;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.squirrels.oboulot.bean.Trajet;
+import com.squirrels.oboulot.bean.User;
+
+/**
+ * Servlet implementation class ProposerTrajet
+ */
+@WebServlet("/formPropositionTrajet")
+public class ProposerTrajet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static String VIEW_PAGES_URL="/WEB-INF/trajet/formulaireProposerTrajet.jsp";
+	public static final String FIELD_DPT = "ptdepart";
+	public static final String FIELD_ARV = "ptarrivee"; 
+	public static final String FIELD_PSG = "ptpassage";
+	public static final String FIELD_DATE = "date"; 
+	public static final String FIELD_HEURE = "heure";
+	public static final String FIELD_NBPLACE = "nbplace";
+	public static final String FIELD_FUMEUR = "fumeur";
+	public static final String FIELD_MUSIQUE = "musique";
+	public static final String SUCCES = "Succès de l'inscription";
+	public static final String ECHEC = "Echec de l'inscription";
+	public static String actionMessage;
+	Trajet newTrajet = null;
+
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProposerTrajet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(VIEW_PAGES_URL).forward( request, response );
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Vérifier les différentes informations
+		//Si non valide, renvoyer sur la même page avec message d'erreur
+		//Si valide, renvoyer vers... et enregistrer trajet dans application
+		
+		HttpSession session = request.getSession();
+		ServletContext application = request.getServletContext();
+		
+		User connectedUser = (User) session.getAttribute("connectedUser");
+		
+		String ptdepart = request.getParameter(FIELD_DPT);
+		String ptarrivee = request.getParameter(FIELD_ARV);
+		String ptpassage = request.getParameter(FIELD_PSG);
+		String date = request.getParameter(FIELD_DATE);
+		String heure = request.getParameter(FIELD_HEURE);
+		String nbplace = request.getParameter(FIELD_NBPLACE);
+		String fumeur = request.getParameter(FIELD_FUMEUR);
+		String musique = request.getParameter(FIELD_MUSIQUE);
+		
+		Map<String, Trajet> trajets = (HashMap<String, Trajet>) application.getAttribute("trajets");
+		if(trajets==null){
+			trajets = new HashMap<String, Trajet>();
+			Trajet admin = new Trajet("2 rue Crucy, Nantes", "Mail Pablo Picasso");
+			trajets.put("Admin", admin);
+			application.setAttribute( "users", trajets );
+		}
+		
+		HashMap<String, String>erreurs = new HashMap<String, String>();
+		HashMap<String, String>form = new HashMap<String, String>();
+		
+		newTrajet = new Trajet(ptdepart,ptarrivee);
+		request.setAttribute("newTrajet", newTrajet);
+		
+		String errPtDepart = validateAdresse(ptdepart) ;
+		if(errPtDepart!=null){
+			erreurs.put(FIELD_DPT, errPtDepart);
+			form.put(FIELD_DPT, null);
+			actionMessage = ECHEC;
+		} else {
+			form.put(FIELD_DPT, ptdepart);
+			actionMessage = SUCCES;
+		}
+		
+		String errPtArrivee = validateAdresse(ptarrivee) ;
+		if(errPtArrivee!=null){
+			erreurs.put(FIELD_ARV, errPtArrivee);
+			form.put(FIELD_ARV, null);
+			actionMessage = ECHEC;
+		} else {
+			form.put(FIELD_ARV, ptarrivee);
+			if (actionMessage.equals(ECHEC)){
+				actionMessage=ECHEC;
+			}
+			else {
+				actionMessage=SUCCES;
+			}
+		}
+		
+		String errPtPassage = validateAdresseFacultative(ptpassage) ;
+		if(errPtPassage!=null){
+			erreurs.put(FIELD_PSG, errPtPassage);
+			form.put(FIELD_PSG, null);
+			actionMessage = ECHEC;
+		} else {
+			form.put(FIELD_PSG, ptpassage);
+			if (actionMessage.equals(ECHEC)){
+				actionMessage=ECHEC;
+			}
+			else {
+				actionMessage=SUCCES;
+			}
+		}
+		
+		String errDate = validateDate(date) ;
+		if(errDate!=null){
+			erreurs.put(FIELD_DATE, errDate);
+			form.put(FIELD_DATE, null);
+			actionMessage = ECHEC;
+		} else {
+			form.put(FIELD_DATE, date);
+			if (actionMessage.equals(ECHEC)){
+				actionMessage=ECHEC;
+			}
+			else {
+				actionMessage=SUCCES;
+			}
+		}
+		
+		String errHeure = validateHeure(heure) ;
+		if(errHeure!=null){
+			erreurs.put(FIELD_HEURE, errHeure);
+			form.put(FIELD_HEURE, null);
+			actionMessage = ECHEC;
+		} else {
+			form.put(FIELD_HEURE, heure);
+			if (actionMessage.equals(ECHEC)){
+				actionMessage=ECHEC;
+			}
+			else {
+				actionMessage=SUCCES;
+			}
+		}
+		
+		String errNbPlace = validateNbPlace(nbplace) ;
+		if(errNbPlace!=null){
+			erreurs.put(FIELD_NBPLACE, errNbPlace);
+			form.put(FIELD_NBPLACE, null);
+			actionMessage = ECHEC;
+		} else {
+			form.put(FIELD_NBPLACE, date);
+			if (actionMessage.equals(ECHEC)){
+				actionMessage=ECHEC;
+			}
+			else {
+				actionMessage=SUCCES;
+			}
+		}
+		
+		form.put(FIELD_FUMEUR, fumeur);
+		form.put(FIELD_MUSIQUE, musique);
+		
+		request.setAttribute("form", form);
+		request.setAttribute("erreurs", erreurs);
+		request.setAttribute("actionMessage", actionMessage);
+		
+		if (actionMessage.equals(SUCCES)){
+			trajets.put(connectedUser.getName(), newTrajet);
+			application.setAttribute( "trajets", trajets );
+			RequestDispatcher dispat = request.getRequestDispatcher("/index.jsp");
+			dispat.forward(request,response);
+		} else{
+			request.setAttribute("errorStatus", false); 
+			RequestDispatcher dispat = request.getRequestDispatcher(VIEW_PAGES_URL);
+			dispat.forward(request,response);
+		}
+	}
+
+
+	private String validateAdresse(String adresse) {
+		if ( adresse != null && adresse.trim().length() != 0 ) {
+			if ( !adresse.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) { 
+				return "Veuillez saisir une adresse valide";
+			} 
+		} else { 
+			return "L'adresse est obligatoire"; 
+		}
+		return null ;
+	}
+	
+	private String validateAdresseFacultative(String adresse) {
+		if ( adresse != null && adresse.trim().length() != 0 ) {
+			if ( !adresse.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) { 
+				return "Veuillez saisir une adresse valide";
+			} 
+		} else { 
+			return null; 
+		}
+		return null ;
+	}
+
+	private String validateDate(String date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private String validateHeure(String heure) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private String validateNbPlace(String nbplace) {
+		if ( nbplace != null && nbplace.trim().length() != 0 ) {
+			try {
+				int i = Integer.parseInt(nbplace);
+				if (0<i && i<5){
+					return null;
+				} else {
+					return "Vous pouvez proposer 1 à 4 places";
+				}
+			} catch (Exception e){
+				return "Veuillez entrer un chiffre numérique";
+			}
+		} else {
+			return "Veuillez entrer le nombre de places que vous proposez";
+		}
+		
+	}
+
+}

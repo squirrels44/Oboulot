335501e63c7426bdceff287e06df105695f40a96
active-admin <active-admin@LAPTOP-6HG7OFNT>
2016-08-25 16:26:46 +0200

    Servlet de déconnexion

---
+++ Oboulot/src/com/squirrels/oboulot/ctrl/Deconnexion.java
@@ -0,0 +1,47 @@
+package com.squirrels.oboulot.ctrl;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.squirrels.oboulot.bean.User;
+import com.squirrels.oboulot.service.UserService;
+
+/**
+ * Servlet implementation class Deconnexion
+ */
+@WebServlet("/Deconnexion")
+public class Deconnexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Deconnexion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession();
+		session.setAttribute("connectedUser", null);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

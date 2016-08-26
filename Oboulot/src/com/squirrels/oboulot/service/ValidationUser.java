d74fc2d4bb1d42cfd7311262677c6154404c085f
active_admin <active_admin@LAPTOP-NL2VEA3I>
2016-08-26 11:56:24 +0200

    Amelioration de scommentaire de ValidationUser

--- Oboulot/src/com/squirrels/oboulot/service/ValidationUser.java
+++ Oboulot/src/com/squirrels/oboulot/service/ValidationUser.java
@@ -4,14 +4,14 @@ import java.util.Map;
 
 import com.squirrels.oboulot.bean.User;
 
-public class ValidationUser {		//Centralisation des methodes de validation d'inscription et de connexion des users
+public class ValidationUser {	//Centralisation des methodes de validation d'inscription et de connexion des users
 
 
 	/////////////////////
 	   //INSCRIPTION//
 	/////////////////////
 
-	//méthode de validation de l'email d'inscription
+	//critère de validation de l'email d'inscription
 	public String validateEmailInscription( String email ){ 
 		if ( email != null && email.trim().length() != 0 ) {
 			if ( !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) { 
@@ -23,7 +23,7 @@ public class ValidationUser {		//Centralisation des methodes de validation d'ins
 		return null ;
 	}
 
-	//méthode de validation du password d'inscription
+	//critère de validation du mot de passe d'inscription
 	public String validatePwdInscription(String pwd1, String pwd2){
 		if (pwd1 != null && pwd1.trim().length() > 5){
 			if (pwd2 != null && pwd2.trim().length() != 0){
@@ -39,7 +39,7 @@ public class ValidationUser {		//Centralisation des methodes de validation d'ins
 		return null;
 	}
 
-	//méthode de validation du téléphone d'inscription
+	//critère de validation du numéro de telephone d'inscription
 	public String validateTelInscription( String tel ){ 
 		if ( tel != null && tel.trim().length() == 10 ) {
 			if ( !tel.startsWith("0")) { 
@@ -51,7 +51,7 @@ public class ValidationUser {		//Centralisation des methodes de validation d'ins
 		return null ;
 	}
 
-	//méthode de validation du nom d'inscription
+	//critere de validation du nom d'inscription
 	public String validateNameInscription( String name, Map<String, User> users ){ 
 		if ( name != null && name.trim().length() != 0 ) {
 			if (users.keySet().contains(name)){
@@ -67,7 +67,7 @@ public class ValidationUser {		//Centralisation des methodes de validation d'ins
 	    //CONNEXION//
 	/////////////////////
 
-	//méthode de validation du nom de connexion
+	//critere de validation du nom de connexion
 	public String validateNameConnexion(String name, String nameInscription){
 		String res = null;
 		if(name ==null || name.trim().length() == 0){
@@ -78,7 +78,7 @@ public class ValidationUser {		//Centralisation des methodes de validation d'ins
 	}
 
 
-	//méthode de validation du password de connexion
+	//critere de validation du mot de passe de connexion
 	public String validatePwdConnexion(String pwd, String pwdInscription) {
 		String res = null;
 		if(pwd ==null || pwd.trim().length() == 0){

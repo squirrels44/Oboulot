94502cfa7f127f05477e094d49799722a79fdde7
active_admin <active_admin@LAPTOP-CKO0CB3B>
2016-08-26 11:53:30 +0200

    Deconnection.java cleaned codé

--- Oboulot/src/com/squirrels/oboulot/ctrl/Deconnexion.java
+++ Oboulot/src/com/squirrels/oboulot/ctrl/Deconnexion.java
@@ -34,12 +34,6 @@ public class Deconnexion extends HttpServlet {
 		dispat.forward(request,response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	
 
 }

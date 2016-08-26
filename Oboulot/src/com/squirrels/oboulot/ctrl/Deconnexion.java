aa04e0c26af8fd48eec51fe7149c1010d66f4c89
active-admin <active-admin@LAPTOP-6HG7OFNT>
2016-08-26 10:34:29 +0200

    refectaring deconnexion

--- Oboulot/src/com/squirrels/oboulot/ctrl/Deconnexion.java
+++ Oboulot/src/com/squirrels/oboulot/ctrl/Deconnexion.java
@@ -1,8 +1,6 @@
 package com.squirrels.oboulot.ctrl;
 
 import java.io.IOException;
-import java.util.Map;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,9 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import com.squirrels.oboulot.bean.User;
-import com.squirrels.oboulot.service.UserService;
 
 /**
  * Servlet implementation class Deconnexion
